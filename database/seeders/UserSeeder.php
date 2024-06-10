4af804ef955db6a7acb544aa257b2c0f8ff5b593
iqbal <emixbal@gmail.com>
2024-06-10 15:42:46 +0700

    seeder user

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use App\Models\User;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::create([
+            'name' => 'Admin',
+            'email' => 'admin@email.com',
+            'password' => Hash::make('admin'),
+        ]);
+
+        User::create([
+            'name' => 'User',
+            'email' => 'user@email.com',
+            'password' => Hash::make('admin'),
+        ]);
+    }
+}
