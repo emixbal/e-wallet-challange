73212122e745cf74681c0ab904cadb9892bec71c
iqbal <emixbal@gmail.com>
2024-06-10 20:09:18 +0700

    add role

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -2,9 +2,10 @@
 
 namespace Database\Seeders;
 
+use App\Models\Role;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Hash;
-use App\Models\User;
 
 class UserSeeder extends Seeder
 {
@@ -15,16 +16,21 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
+        $adminRole = Role::where('name', 'Admin')->first();
+        $userRole = Role::where('name', 'User')->first();
+
         User::create([
             'name' => 'Admin',
             'email' => 'admin@email.com',
             'password' => Hash::make('aaaaaaaa'),
+            'role_id' => $adminRole->id,
         ]);
 
         User::create([
             'name' => 'User',
             'email' => 'user@email.com',
             'password' => Hash::make('aaaaaaaa'),
+            'role_id' => $userRole->id,
         ]);
     }
 }
