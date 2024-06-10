12d3ace9225b747209c9c61ec0955162c94c075e
iqbal <emixbal@gmail.com>
2024-06-10 11:35:39 +0700

    fetch data

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -1,4 +1,37 @@
 @extends('layout')
+@section('css')
+    <style>
+        body {
+            background-color: #f8f9fa;
+        }
+
+        .wallet-balance {
+            font-size: 2rem;
+            font-weight: bold;
+            margin: 20px 0;
+        }
+
+        .transaction {
+            border-bottom: 1px solid #dee2e6;
+            padding: 15px 0;
+            margin: 0 10px;
+        }
+
+        .transaction:last-child {
+            border-bottom: none;
+        }
+
+        .btn-float {
+            position: fixed;
+            bottom: 20px;
+            right: 20px;
+            border-radius: 50%;
+            padding: 15px 20px;
+            font-size: 1.5rem;
+            margin-left: 10px;
+        }
+    </style>
+@endsection
 
 @section('content')
     <div class="text-center mt-5">
@@ -7,44 +40,7 @@
     <div class="text-center wallet-balance">
         $1,250.00
     </div>
-    <div class="list-group mt-4">
-        <div class="list-group-item transaction">
-            <div class="d-flex justify-content-between m-1">
-                <div>
-                    <strong>Amazon Purchase</strong>
-                    <div>March 20, 2024</div>
-                </div>
-                <div class="text-danger">- $150.00</div>
-            </div>
-        </div>
-        <div class="list-group-item transaction">
-            <div class="d-flex justify-content-between m-1">
-                <div>
-                    <strong>Salary</strong>
-                    <div>March 15, 2024</div>
-                </div>
-                <div class="text-success">+ $1,500.00</div>
-            </div>
-        </div>
-        <div class="list-group-item transaction">
-            <div class="d-flex justify-content-between m-1">
-                <div>
-                    <strong>Starbucks</strong>
-                    <div>March 12, 2024</div>
-                </div>
-                <div class="text-danger">- $25.00</div>
-            </div>
-        </div>
-        <div class="list-group-item transaction">
-            <div class="d-flex justify-content-between m-1">
-                <div>
-                    <strong>Electric Bill</strong>
-                    <div>March 10, 2024</div>
-                </div>
-                <div class="text-danger">- $100.00</div>
-            </div>
-        </div>
-    </div>
+    <div class="list-group mt-4" id="transactions"></div>
 
     <!-- Pay Button -->
     <button type="button" class="btn btn-primary btn-float" data-toggle="modal" data-target="#payModal">
