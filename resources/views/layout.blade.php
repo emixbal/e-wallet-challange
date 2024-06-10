1b1c86c0400843a81544876e6d57073a2d7e2287
iqbal <emixbal@gmail.com>
2024-06-10 10:26:54 +0700

    add logout

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -6,37 +6,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>{{ $page['parent_title'] }} - {{ $page['title'] }}</title>
     <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
-    <style>
-        body {
-            background-color: #f8f9fa;
-        }
-
-        .wallet-balance {
-            font-size: 2rem;
-            font-weight: bold;
-            margin: 20px 0;
-        }
-
-        .transaction {
-            border-bottom: 1px solid #dee2e6;
-            padding: 15px 0;
-            margin: 0 10px;
-        }
-
-        .transaction:last-child {
-            border-bottom: none;
-        }
-
-        .btn-float {
-            position: fixed;
-            bottom: 20px;
-            right: 20px;
-            border-radius: 50%;
-            padding: 15px 20px;
-            font-size: 1.5rem;
-            margin-left: 10px;
-        }
-    </style>
     @yield('css')
 </head>
 
@@ -56,6 +25,9 @@
                 <li class="nav-item">
                     <a class="nav-link" href="#" data-toggle="modal" data-target="#topUpModal">Top Up</a>
                 </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#" id="btn_logout">Logout</a>
+                </li>
             </ul>
         </div>
     </nav>
@@ -69,10 +41,32 @@
         var base_app_url = "{{ url('') }}";
     </script>
 
-    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://code.jquery.com/jquery-3.7.1.js" integrity="sha256-eKhayi8LEQwp4NKxN+CfCh+3qOVUtJn3QNZ0TciWLP4="
+        crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
     @yield('js')
+
+    <script>
+        $(document).ready(function() {
+            // Check if access token already exists
+            var accessToken = localStorage.getItem('access_token');
+            if (!accessToken) {
+                window.location.href = "{{ route('login') }}";
+                return;
+            }
+
+            // Handle logout button click
+            $("#btn_logout").on("click", function() {
+                // Remove the access token from local storage
+                localStorage.removeItem('access_token');
+
+                // Redirect to the login page
+                window.location.href = "{{ route('login') }}";
+                return
+            });
+        });
+    </script>
 </body>
 
 </html>
